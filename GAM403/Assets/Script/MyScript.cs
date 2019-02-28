796f94d18560d637115b0a91c874ee88721db2b7
LSnelling <47873896+LSnelling@users.noreply.github.com>
2019-03-01 09:22:57 +1100

    Starting Uni Programing Unity Commit
    This was my Starting Work

---
+++ GAM403/Assets/Script/MyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{// Pay Attention in class you stupid c***
+
+    public int ammo = 999999999;
+    public int heath = 100;
+    public float accuracy = 15.5f;
+    public float playerSpeed = 0.5f;
+    public float speedBurst = -0.01f;
+    public string player = "Luke";
+
+    public Vector3 starting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammo = ammo + ammo;
+        Debug.Log("My Ammo is: " + ammo);
+        transform.position = starting;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerSpeed = playerSpeed + speedBurst; // Increases our player speed
+        transform.Translate(Vector3.forward * playerSpeed); //Moves us forward in the game test mode
+    }
+}
