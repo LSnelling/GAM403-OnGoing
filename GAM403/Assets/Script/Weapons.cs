3d94a39828614bcf8dfef26d1ebf0d68c4b0c94d
LSnelling <47873896+LSnelling@users.noreply.github.com>
2019-03-08 11:55:46 +1100

    Week4WeaponLoops

---
+++ GAM403/Assets/Script/Weapons.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public GameObject[] weapons;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        ChangeWeapons(0);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            ChangeWeapons(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            ChangeWeapons(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            ChangeWeapons(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            ChangeWeapons(3);
+    }
+
+    void ChangeWeapons(int weaponNumber) //This is a loop
+   {
+        for(int i = 0; i < weapons.Length; i++)
+        {
+            if (i != weaponNumber)
+                weapons[i].SetActive(false);
+            else
+                weapons[i].SetActive(true);
+        }
+   
+   }
+}
